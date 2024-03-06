f56343bda38ed3f65c029904f8cd688a6b931b67
Kevin Muthami <153747395+MuthamiKN@users.noreply.github.com>
2024-03-07 01:03:29 +0300

    update

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,138 +1,134 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
-function myMenuFunction(){
-    let menuBtn = document.getElementById("myNavMenu");
-  
-    if(menuBtn.className === "nav-menu"){
+function myMenuFunction() {
+  let menuBtn = document.getElementById("myNavMenu");
+
+  if (menuBtn.className === "nav-menu") {
       menuBtn.className += " responsive";
-    } else {
+  } else {
       menuBtn.className = "nav-menu";
-    }
   }
-  
-  /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-  window.onscroll = function() {headerShadow()};
-  
-  function headerShadow() {
-    const navHeader =document.getElementById("header");
-  
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-  
+}
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+window.onscroll = function() {
+  headerShadow();
+};
+
+function headerShadow() {
+  const navHeader = document.getElementById("header");
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+
       navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
       navHeader.style.height = "70px";
       navHeader.style.lineHeight = "70px";
-  
-    } else {
-  
+
+  } else {
+
       navHeader.style.boxShadow = "none";
       navHeader.style.height = "90px";
       navHeader.style.lineHeight = "90px";
-  
-    }
+
   }
-  
-  /* ----- TYPING EFFECT ----- */
-  let typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Youtuber","Developer"],
-    loop : true,
-    typeSpeed : 100,
-    backSpeed : 80,
-    backDelay : 2000
-  })
-  
-  /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-  const sr = ScrollReveal({
-        origin: 'top',
-        distance: '80px',
-        duration: 2000,
-        reset: true
-  })
-  
-  
-  /* -- HOME -- */
-  sr.reveal('.featured-text-card',{})
-  sr.reveal('.featured-name',{delay: 100})
-  sr.reveal('.featured-text-info',{delay: 200})
-  sr.reveal('.featured-text-btn',{delay: 200})
-  sr.reveal('.social_icons',{delay: 200})
-  sr.reveal('.featured-image',{delay: 300})
-  
-  /* -- PROJECT BOX -- */
-  sr.reveal('.project-box',{interval: 200})
-  
-  /* -- HEADINGS -- */
-  sr.reveal('.top-header',{})
-  
-  /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
-  
-  /* -- ABOUT INFO & CONTACT INFO -- */
-  const srLeft = ScrollReveal({
+}
+
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText", {
+  strings: ["Designer", "Youtuber", "Developer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000
+});
+
+/* ----- SCROLL REVEAL ANIMATION ----- */
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card', {});
+sr.reveal('.featured-name', { delay: 100 });
+sr.reveal('.featured-text-info', { delay: 200 });
+sr.reveal('.featured-text-btn', { delay: 200 });
+sr.reveal('.social_icons', { delay: 200 });
+sr.reveal('.featured-image', { delay: 300 });
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box', { interval: 200 });
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header', {});
+
+/* ----- SCROLL REVEAL LEFT_RIGHT ANIMATION ----- */
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
   origin: 'left',
   distance: '80px',
   duration: 2000,
   reset: true
-  })
-  
-  srLeft.reveal('.about-info',{delay: 100})
-  srLeft.reveal('.contact-info',{delay: 100})
-  
-  /* -- ABOUT SKILLS & FORM BOX -- */
-  const srRight = ScrollReveal({
+});
+
+srLeft.reveal('.about-info', { delay: 100 });
+srLeft.reveal('.contact-info', { delay: 100 });
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
   origin: 'right',
   distance: '80px',
   duration: 2000,
   reset: true
-  })
-  
-  srRight.reveal('.skills-box',{delay: 100})
-  srRight.reveal('.form-control',{delay: 100})
-  
-  /* ----- CHANGE ACTIVE LINK ----- */
-  
-  const sections = document.querySelectorAll('section[id]')
-  function scrollActive() {
+});
+
+srRight.reveal('.skills-box', { delay: 100 });
+srRight.reveal('.form-control', { delay: 100 });
+
+/* ----- CHANGE ACTIVE LINK ----- */
+const sections = document.querySelectorAll('section[id]');
+function scrollActive() {
   const scrollY = window.scrollY;
-  
-  sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
-  
-    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-  
-        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-  
-    }  else {
-  
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-  
-    }
-  })
-  }
 
-  var icon = document.getElementById("icon");
+  sections.forEach(current => {
+      const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50,
+          sectionId = current.getAttribute('id');
 
-  icon.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.bodyy.classList.contains("dark-theme")){
-      icon.src="sun.png";
-      
-    }else{
-      icon.src=("moon.png")
-    }
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+
+          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+
+      } else {
+
+          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+
+      }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+/* ----- ICON CLICK HANDLER ----- */
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+      icon.src = "sun.png";
+  } else {
+      icon.src = "moon.png";
   }
-  
-  window.addEventListener('scroll', scrollActive)
-  document.getElementById('download-btn').addEventListener('click', function() {
-    // Create an anchor element
-    var a = document.createElement('a');
-    // Set the href attribute to the URL of the document to be downloaded
-    a.href = 'C:\Users\mutha\OneDrive\Documents\GitHub\MuthamiKN\Portfolio\Resume.docx'; // Replace 'path/to/your/document.pdf' with the actual path to your document
-    // Set the download attribute to specify the filename users will see when they download the document
-    a.download = 'resume.docx'; // Replace 'document.pdf' with the desired filename
-    // Append the anchor element to the body
-    document.body.appendChild(a);
-    // Trigger a click event on the anchor element
-    a.click();
-    // Remove the anchor element from the body
-    document.body.removeChild(a);
+};
+
+/* ----- DOWNLOAD BUTTON HANDLER ----- */
+document.getElementById('download-btn').addEventListener('click', function() {
+  // Replace 'path/to/your/document.pdf' with the actual path to your document
+  var resumeURL = 'path/to/your/document.docx'; 
+  var a = document.createElement('a');
+  a.href = resumeURL;
+  a.download = 'resume.docx';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 });
